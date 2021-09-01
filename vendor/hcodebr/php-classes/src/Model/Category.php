067cf06c01c4a2f483e32cb3a9961a29e893a137
Andre Luis <andreluis2608@gmail.com>
2021-09-01 14:02:46 -0300

    site - categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -33,6 +33,8 @@ class Category extends Model
             ));
 
             $this->setData($result[0]);
+
+            Category::updateFile();
     }
 
     /**
@@ -65,7 +67,21 @@ class Category extends Model
             ":idcategory"=>$this->getidcategory()
         ));
 
+        Category::updateFile();
+    }
 
+    public static function updateFile()
+    {
+        $categories= Category::listAll();
+
+        $html = array();
+
+        foreach($categories as $row)
+        {
+            array_push($html,"<li><a href='/categories/".$row["idcategory"]."'>".$row["descategory"]."</a></li>");
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html",implode("",$html));
     }
 
 }
